1b3a2c30312f52cf10f8d1e14f5a67f729794873
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-07 00:01:57 +0530

    Class Note done

--- Webpack (Day-1)/Class Notes/src/index.js
+++ Webpack (Day-1)/Class Notes/src/index.js
@@ -1,6 +1,19 @@
 const {sum,multi} = require("./calc");
 
+import "./index.css";
 
 console.log(sum(53,16))
 console.log(multi(5,5))
 console.log("Hello World !")
+
+
+const root = document.getElementById('root');
+
+const h3 = document.createElement('h3');
+h3.textContent = "This is the Intro to Webpackage Lecture.";
+h3.classList.add("redtext");
+// Above Style can also be written as h1.setAtrribute("class","redtext");
+
+root.append(h3); // Can also be Written as root.appendChild(h3);
+
+// ---------- To run CSS file in js file we need to packages - style-loader & css-loader
--- Webpack (Day-1)/Class Notes/webpack.config.js
+++ Webpack (Day-1)/Class Notes/webpack.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-    entry : './src/index.js',
-    output : {
 
-    }
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(".", "build"), //Folder created with name "build"
+        filename: "bundle.js"  //File will be cretaed with the name "bundle"
+    },
+    mode: "development", // There are two mode, 1st "development" 2nd "production"
+    module: {
+        rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+    },
 };
