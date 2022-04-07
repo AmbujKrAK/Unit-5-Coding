26c8471e732a583dc40964b1c03951043af3f390
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-08 01:20:29 +0530

    Added React to project folder

---
+++ React Intro (Day-2)/Assignment/react-intro/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">Testing Hello World.
+    <button>Test</button>
+    <input />
+    <img src = "https://picsum.photos/536/354" alt = ""/>
+    </div>
+  );
+}
+
+export default App;
