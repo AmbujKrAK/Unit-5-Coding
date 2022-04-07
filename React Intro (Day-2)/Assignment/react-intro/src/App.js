b5e6e804301f9111a73a28a3f185608d129fe966
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-08 02:12:26 +0530

    class note for react added

--- React Intro (Day-2)/Assignment/react-intro/src/App.js
+++ React Intro (Day-2)/Assignment/react-intro/src/App.js
@@ -1,12 +1,34 @@
 
+import './App.css'
+
+
+
 function App() {
+
+  const food = "Pizza";
+  const age = 18;
+  const skills = ["one", "two", "three", "four", "five"]
+
+
   return (
-    <div className="App">Testing Hello World.
-    <button>Test</button>
-    <input />
-    <img src = "https://picsum.photos/536/354" alt = ""/>
+    <div className="App">
+      <h1>REACT MOBILE OS</h1>
+      <div>My fav food is {food}</div>
+      <div> You are {age >= 18 ? "Eligible" : "Not Eligible"} for License.</div>
+      <div>
+          {skills.map((e) => {
+            return <Skill skills={e}/>
+          })}
+      </div>
     </div>
   );
 }
+
+//Components
+
+function Skill({skills}){
+  return <p className="text"> Skill: {skills}</p>
+}
+
 
 export default App;
