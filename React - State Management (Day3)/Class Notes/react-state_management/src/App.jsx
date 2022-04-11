68fb0e39654013abde1fea0b01ee93242398b87a
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-11 15:34:08 +0530

    blank

--- React - State Management (Day3)/Class Notes/react-state_management/src/App.jsx
+++ React - State Management (Day3)/Class Notes/react-state_management/src/App.jsx
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data = "Hello World!";
   return (
     <div className="App">
-     testing
+     <First data={data}/>
+    <Second/>
     </div>
   );
+}
+
+function First({data}){
+  console.log("Recevied from Parent",{data})
+  return <div>First Child,{data}</div>
+}
+
+const Second = () => {
+  return <div>Second Child</div>
 }
 
 export default App;
