7f33c35b914027b8870c373d7cd37bd39c1a242c
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-11 16:41:54 +0530

    Sending from Child to parent

--- React - State Management (Day3)/Class Notes/react-state_management/src/App.jsx
+++ React - State Management (Day3)/Class Notes/react-state_management/src/App.jsx
@@ -1,23 +1,40 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Todo } from './components/todo';
+import { TodoInput } from './components/todoInput';
 
 function App() {
-  const data = "Hello World!";
+  // const [message, setMessage] = useState("");
+
+  // const getData = (data) => {
+  //   console.log("Data:", data)
+  //   setMessage(data)
+
+  // };
+
   return (
     <div className="App">
-     <First data={data}/>
-    <Second/>
+
+      {/* <First dataFn={getData} />
+      <Second message={message}/> */}
+   
+      <Todo/>
+
     </div>
   );
 }
 
-function First({data}){
-  console.log("Recevied from Parent",{data})
-  return <div>First Child,{data}</div>
-}
+// function First({ dataFn }) {
+//   const data = "Hello World !"
+//   return <div>First Child<button onClick={()=>{dataFn(data)}}>Send Data</button>
+//   </div>
+// }
 
-const Second = () => {
-  return <div>Second Child</div>
-}
+// const Second = ({message}) => {
+//   console.log("message:",{message})
+
+//   return <div>Second Child,{message}</div>
+// }
 
 export default App;
---
+++ React - State Management (Day3)/Class Notes/react-state_management/src/components/todoInput.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const TodoInput = ({getData}) => {
+    const [text, setText] = useState("");
+
+    return (
+        <div>
+            <input onChange={(e) => {
+                // console.log(e.target.value);
+                setText(e.target.value);
+            }}
+                type="text"
+                placeholder="Enter Task"
+            />
+            <button onClick={()=>{
+                getData(text)
+            }}>Add Todo</button>
+        </div>
+    );
+};
