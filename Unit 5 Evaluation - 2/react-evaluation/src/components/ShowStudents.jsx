dec6911d2577920cf03fdfe4a5aa64d6c0799db0
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-18 14:55:32 +0530

    added data on Dom

--- Unit 5 Evaluation - 2/react-evaluation/src/components/ShowStudents.jsx
+++ Unit 5 Evaluation - 2/react-evaluation/src/components/ShowStudents.jsx
@@ -1,16 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ShowStudents = () => {
-useEffect(()=>{
-  async function getData(){
-    var data = await fetch("http://localhost:8080/students")
-    var res = await data.json();
-  }
-})
-
-
-
-
+  const [studentData, setStudentData] = useState([]);
+  useEffect(()=>{
+    async function getdata(){
+      var data = await fetch("http://localhost:8080/students")
+      var res = await data.json();
+      setStudentData(res)
+    }
+    getdata()
+  },[])
   return (
     <div>
       <div className="controls">
@@ -54,16 +53,18 @@ useEffect(()=>{
         </thead>
         <tbody className="tbody">
           {/* populate all rows like below: */}
-          <tr className="row">
-            <td className="first_name"></td>
-            <td className="last_name"></td>
-            <td className="email"></td>
-            <td className="gender"></td>
-            <td className="age"></td>
-            <td className="tenth_score"></td>
-            <td className="twelth_score"></td>
-            <td className="preferred_branch"></td>
+          {studentData.map((e)=>{
+            return <tr className="row">
+            <td className="first_name">{e.first_name}</td>
+            <td className="last_name">{e.last_name}</td>
+            <td className="email">{e.email}</td>
+            <td className="gender">{e.gender}</td>
+            <td className="age">{e.age}</td>
+            <td className="tenth_score">{e.tenth_score}</td>
+            <td className="twelth_score">{e.twelth_score}</td>
+            <td className="preferred_branch">{e.preferred_branch}</td>
           </tr>
+          })}
         </tbody>
       </table>
     </div>
