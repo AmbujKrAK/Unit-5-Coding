06594038be6c6f63ab20838e047d2ca147914484
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-04-25 16:45:34 +0530

    updated

--- Unit 5 Evaluation - 3/react-eval3/src/components/Admin.jsx
+++ Unit 5 Evaluation - 3/react-eval3/src/components/Admin.jsx
@@ -1,35 +1,56 @@
 import { useState } from "react";
 
 export const Admin = () => {
-  const [data,  setdata] = useState({
+  const [data,  setData] = useState();
 
-  })
+  const handleChange = (e) => {
+   
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    
+}
+const handleSubmit = async(e) => {
+  e.preventDefault();
+
+  await fetch("http://localhost:8080/employee", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+    .then((res) => res.json())
+    console.log(data);
+ 
+}
   
   return (
-    <form className="createEmployee">
-      <input type="text" placeholder="Employee Name" name="employee_name" />
-      <input type="text" placeholder="Employee id" name="employee_id" />
-      <select name="title">
+    <form className="createEmployee" onSubmit={handleSubmit}>
+      <input type="text" placeholder="Employee Name" name="employee_name" onChange={handleChange} />
+      <input type="text" placeholder="Employee id" name="employee_id" onChange={handleChange}/>
+      <select name="title" onChange={handleChange}>
         <option value="intern">Intern</option>
         <option value="Jr Software Developer">Jr Software Developer</option>
         <option value="Sr Software Developer">Sr Software Developer</option>
         <option value="Team Lead">Team Lead</option>
       </select>
-      <input type="number" placeholder="Salary" name="salary" />
-      <input type="text" placeholder="Image" name="image" />
-      <input type="text" placeholder="User Name" name="username" />
-      <input type="password" placeholder="Password" name="password" />
+      <input type="number" placeholder="Salary" name="salary" onChange={handleChange}/>
+      <input type="text" placeholder="Image" name="image" onChange={handleChange}/>
+      <input type="text" placeholder="User Name" name="username" onChange={handleChange}/>
+      <input type="password" placeholder="Password" name="password" onChange={handleChange}/>
       <input
         type="text"
         placeholder="Enter tasks separated by commas"
         name="tasks"
       />
-      <select name="status" id="status">
+      <select name="status" id="status"onChange={handleChange}>
         <option value="">Select Status</option>
         <option value="terminated">Terminated</option>
         <option value="working">Working</option>
       </select>
-      <select name="team" id="team">
+      <select name="team" id="team" onChange={handleChange}>
         <option value="">Select team</option>
         <option value="frontend">Frontend</option>
         <option value="backend">Backend</option>
--- Unit 5 Evaluation - 3/react-eval3/src/components/Login.jsx
+++ Unit 5 Evaluation - 3/react-eval3/src/components/Login.jsx
@@ -1,8 +1,35 @@
+import { useContext, useEffect } from "react";
+import { LoginContext } from "../context/Logincontex";
 export const Login = () => {
-  //  use reqres to log user in.
+  const {handlelogin}= useContext(LoginContext)
+  var data = {
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+  };
 
+   const handle =(e)=>{
+    e.preventDefault();
+    handlelogin()
+   }
+  useEffect(()=>{
+    const handleSubmit = async(e) => {
+      e.preventDefault();
+    
+      await fetch("https://reqres.in/api/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((res) => res.json())
+        
+     console.log(data)
+    }
+  },[])
+   
   return (
-    <form className="loginform">
+    <form className="loginform" onSubmit={handle}>
       <input
         name="username"
         type="text"
